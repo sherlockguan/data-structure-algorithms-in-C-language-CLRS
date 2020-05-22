6b571cba4e0e21ea80937c2553fbb9dd2a29f37e
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-05-22 11:48:37 +0900

    Create AVL.h

---
+++ Binary tree/AVL_tree_rotation/AVL.h
@@ -0,0 +1,29 @@
+#pragma once
+template<class T>
+struct TreeNode
+{
+        data;
+        TreeNode* pLeft;
+        TreeNode* pRight;
+        int height;
+}
+
+template<class T>
+class AVLTree
+{
+        TreeNode<T> *pRoot;
+ public:
+        AVLTree(){pRoot =NULL;}
+        ~AVLTree(){};
+        void insert(const T&data);//引用防止临时对象的产生
+ private:
+ //插入完毕后还要做旋转 所以要找到对应的子树
+        void insertNodeToTree(TreeNode<T>*&root, const T&data);
+        
+}
+
+template<class T>
+void AVLTree<T>::insert(const T&data)
+{
+
+}
